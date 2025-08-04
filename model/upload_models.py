ea3d30462a7974c67a2491cb9e506b819035bf17
Abdelilah Elgallati <abdelilah.elgallati6@gmail.com>
2025-08-04 23:07:03 +0100

    upload models

--- model/upload_models.py
+++ model/upload_models.py
@@ -1,22 +1,11 @@
 from huggingface_hub import upload_file
+import os
+
 
 upload_file(
     path_or_fileobj="similarity.pkl",
     path_in_repo="similarity.pkl",
     repo_id="AbdelilahElgallati/Movie-Recommender-System",
     repo_type="space",
-    
-    token="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"  
+    token = os.getenv("HF_TOKEN")
 )
-
-
-
-# from huggingface_hub import create_repo
-
-# create_repo(
-#     repo_id="AbdelilahElgallati/Movie-Recommender-System",
-#     repo_type="space",
-#     space_sdk="gradio",
-#     private=False,
-#     token="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"
-# )
